bc2cab594aeccbf64ccb1244cb5d6e7b22bbbcc8
WWZ <zeing@outlook.com>
2019-04-19 21:06:56 +0700

    feat: adjust style

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,10 +4,10 @@ class Experience extends Component {
     return (
       <div className="flex flex-col px-8">
        <h1 className="title" >Experience</h1>
-        <div className="flex flex-col">
-          <div className="flex justify-between w-full">
-              <span className="">Co-operative Education, <p className="font-bold">SmileFOKUS (Thailand) Company Limited</p></span>
-              <span>June 13, 2016 to December 30, 2016</span>
+        <div className="flex flex-col w-full">
+          <div className="flex flex-wrap justify-between w-full">
+              <span className="w-full md:w-1/2">Co-operative Education, <p className="font-bold">SmileFOKUS (Thailand) Company Limited</p></span>
+              <span className="pb-2 pt-0 sm:pb-4 sm:pt-4 w-full md:w-1/2">June 13, 2016 to December 30, 2016</span>
           </div>
           <ul>
             <li>Brainstorm, Get Requirement</li>
@@ -15,16 +15,16 @@ class Experience extends Component {
             <li>Design Architecture</li>
             <li>Develop Website Application Wtih Node.js and Express.js</li>
           </ul>
-          <div className="flex justify-between w-full">
-              <span className="">Automate Tester, <p className="font-bold">SmileFOKUS (Thailand) Company Limited</p></span>
-              <span>Jan 16, 2017 to May 29, 2017</span>
+          <div className="flex flex-wrap justify-between w-full">
+              <span lassName="w-full md:w-1/2">Automate Tester, <p className="font-bold">SmileFOKUS (Thailand) Company Limited</p></span>
+              <span className="pb-2 pt-0 sm:pb-4 sm:pt-4 w-full md:w-1/2">Jan 16, 2017 to May 29, 2017</span>
           </div>
           <ul>
             <li>Testing Website by Robot Framework with Python</li>          
           </ul>
-          <div className="flex justify-between w-full">
+          <div className="flex flex-wrap justify-between w-full">
               <span className="">Full-Stack developer, <p className="font-bold">Telemedica co, ltd. ( OOCA )</p></span>
-              <span>July 11, 2017 to Present</span>
+              <span className="pb-2 pt-0 sm:pb-4 sm:pt-4">July 11, 2017 to Present</span>
           </div>
           <ul>
             <li>Develop all projects in OOCA</li>          
