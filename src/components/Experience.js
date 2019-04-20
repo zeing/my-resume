cf919bdfc86b1c8bac2e4ef4ca84a6b0f6edd81c
WWZ <zeing@outlook.com>
2019-04-21 00:43:12 +0700

    feat : add social link

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,7 +6,7 @@ class Experience extends Component {
        <h1 className="title" >Experience</h1>
         <div className="flex flex-col w-full">
           <div className="flex flex-wrap justify-between w-full">
-              <span className="w-full md:w-1/2 pb-0 sm:pb-4">Co-operative Education, <p className="font-bold">SmileFOKUS (Thailand) Company Limited</p></span>
+              <span className="w-full md:w-1/2 pb-0 sm:pb-4 font-bold">Co-operative Education, <p className="">SmileFOKUS (Thailand) Company Limited</p></span>
               <span className="date-range">June 13, 2016 to December 30, 2016</span>
           </div>
           <ul>
@@ -16,14 +16,14 @@ class Experience extends Component {
             <li>Develop Website Application Wtih Node.js and Express.js</li>
           </ul>
           <div className="flex flex-wrap w-full">
-              <span className="w-full md:w-1/2 pb-0 sm:pb-4">Automate Tester, <p className="font-bold">SmileFOKUS (Thailand) Company Limited</p></span>
+              <span className="w-full md:w-1/2 pb-0 sm:pb-4 font-bold">Automate Tester, <p className="">SmileFOKUS (Thailand) Company Limited</p></span>
               <span className="date-range">Jan 16, 2017 to May 29, 2017</span>
           </div>
           <ul>
             <li>Testing Website by Robot Framework with Python</li>          
           </ul> 
           <div className="flex flex-wrap justify-between w-full">
-              <span className="w-full md:w-1/2 pb-0 sm:pb-4">Full-Stack developer, <p className="font-bold">Telemedica co, ltd. ( OOCA )</p></span>
+              <span className="w-full md:w-1/2 pb-0 sm:pb-4 font-bold">Full-Stack developer, <p className="">Telemedica co, ltd. ( OOCA )</p></span>
               <span className="date-range">July 11, 2017 to Present</span>
           </div>
           <ul>
